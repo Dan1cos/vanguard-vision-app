5192e43dcf0f0c75fb35cdeca1ccc8be2214b363
makche <>
2025-11-14 21:11:54 +0100

    map added

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,3 +1,17 @@
-export default function TabTwoScreen() {
-  return <div>Empty tab</div>;
+import { OrdnanceMap } from 'components/ordnance-map'; // Adjust path if needed
+import React, { JSX } from 'react';
+import { StyleSheet, View } from 'react-native';
+export default function TabTwoScreen(): JSX.Element {
+  return (
+    <View style={styles.container}>
+      <OrdnanceMap />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
